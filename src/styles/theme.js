384c322f555efeb10080993c43ad04c372ba4bed
Lucas Wendell <lucaswhendellf@gmail.com>
2023-03-06 16:24:54 -0300

    STYLE: creating global style

---
+++ src/styles/theme.js
@@ -0,0 +1,84 @@
+export const theme = {
+	font: "'League Spartan', sans-serif",
+	mainTheme: {
+		toggleColor: "hsl(6, 63%, 50%)",
+		backgrounds: {
+			mainBg: "hsl(222, 26%, 31%)",
+			keypadBg: "hsl(223, 31%, 20%)",
+			screenBg: "hsl(224, 36%, 15%)",
+		},
+		keys: {
+			specialKey: {
+				reset: {
+					colorBg: "hsl(225, 21%, 49%)",
+					shadowBg: "hsl(224, 28%, 35%)",
+				},
+				equal: {
+					colorBg: "hsl(6, 63%, 50%)",
+					shadowBg: "hsl(6, 70%, 34%)",
+				},
+			},
+			keyBg: "hsl(30, 25%, 89%)",
+			keyShadow: "hsl(28, 16%, 65%)",
+		},
+		textColors: {
+			primary: "hsl(221, 14%, 31%)",
+			secondary: "hsl(0, 0%, 100%)",
+			tertiary: "hsl(0, 0%, 100%)",
+		},
+	},
+	lightTheme: {
+		toggleColor: "hsl(25, 98%, 40%)",
+		backgrounds: {
+			mainBg: "hsl(0, 0%, 90%)",
+			keypadBg: "hsl(0, 5%, 81%)",
+			screenBg: "hsl(0, 0%, 93%)",
+		},
+		keys: {
+			specialKey: {
+				reset: {
+					colorBg: "hsl(185, 42%, 37%)",
+					shadowBg: "hsl(185, 58%, 25%)",
+				},
+				equal: {
+					colorBg: "hsl(25, 98%, 40%)",
+					shadowBg: "hsl(25, 99%, 27%)",
+				},
+			},
+			keyBg: "hsl(45, 7%, 89%)",
+			keyShadow: "hsl(35, 11%, 61%)",
+		},
+		textColors: {
+			primary: "hsl(60, 10%, 19%)",
+			secondary: "hsl(0, 0%, 100%)",
+			tertiary: "hsl(0, 0%, 100%)",
+		},
+	},
+	purpleTheme: {
+		toggleColor: "hsl(176, 100%, 44%)",
+		backgrounds: {
+			mainBg: "hsl(268, 75%, 9%)",
+			keypadBg: "hsl(268, 71%, 12%)",
+			screenBg: "hsl(268, 71%, 12%)",
+		},
+		keys: {
+			specialKey: {
+				reset: {
+					colorBg: "hsl(281, 89%, 26%)",
+					shadowBg: "hsl(285, 91%, 52%)",
+				},
+				equal: {
+					colorBg: "hsl(176, 100%, 44%)",
+					shadowBg: "hsl(177, 92%, 70%)",
+				},
+			},
+			keyBg: "hsl(268, 47%, 21%)",
+			keyShadow: "hsl(290, 70%, 36%)",
+		},
+		textColors: {
+			primary: "hsl(52, 100%, 62%)",
+			secondary: "hsl(198, 20%, 13%)",
+			tertiary: "hsl(0, 0%, 100%)",
+		},
+	},
+};
